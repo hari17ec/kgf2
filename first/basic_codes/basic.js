2bfc5e170cfe7bdae1dacc3357c6084d3d64d762
hari17ec <rch2hariprasad@gmail.com>
2021-08-19 22:27:47 +0530

    errorcommit

---
+++ first/basic_codes/basic.js
@@ -0,0 +1,27 @@
+/*let n=4568
+function reverse(n){
+    let rev_num=0;
+    while(n>0)
+    {
+        rev_num=rev_num*10+n%10;
+        n=Math.floor(n/10);
+    }
+    return rev_num;
+}
+console.log(reverse(n));*/
+//8654
+
+/*function fibbonaci(n)
+{
+    var n1=0;
+    var n2=1;
+    var sum;//error code
+    for(i=0;i<n;i++){
+        sum=n1+n2;
+        n1=n2
+        n2=sum
+    }
+    return n2
+    
+}
+console.log(fibbonaci(9))*/
--- one.js
+++
@@ -1,3 +0,0 @@
-if ('Kumar') {
-    console.log("Good Morning");
-  }
