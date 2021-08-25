51d56a3917e721c081145946334522af92b1b301
hari17ec <rch2hariprasad@gmail.com>
2021-08-25 21:32:09 +0530

    arr

---
+++ first/basic_codes/array.js
@@ -0,0 +1,38 @@
+// let array = ['mithun',"hari","kgf1","kgf2"]
+// console.log(array)
+// //prints array [ 'mithun', 'hari', 'kgf1', 'kgf2' ]
+
+// let array = ['mithun',"hari","kgf1","kgf2"]
+// console.log(array)
+// let x = array.sort();
+// console.log(x)//sorts ascending by default,[ 'hari', 'kgf1', 'kgf2', 'mithun' ]
+
+// let array1 = ['a',"b","c","d"]
+// let array2 = ['e',"f","g","h"]
+// let arr = array1.concat(array2)
+// console.log(arr)
+// //[
+//     'a', 'b', 'c',
+//     'd', 'e', 'f',
+//     'g', 'h'
+//   ]
+
+// let array1 = ['a',"b","c","d"]
+// let array2 = ['e',"f","g","h"]
+// let array3 = ['e',"f","g","h"]
+// let arr = array1.concat(array2,array3)
+// console.log(arr)
+// //[
+//     'a', 'b', 'c', 'd',
+//     'e', 'f', 'g', 'h',
+//     'e', 'f', 'g', 'h'
+//   ]
+
+// let array = ['mithun',"hari","kgf1","kgf2","a","b"];
+// console.log(array.copyWithin(0,2,5));//[ 'kgf1', 'kgf2', 'a', 'kgf2', 'a', 'b' ]
+// console.log(array.copyWithin(0,3));//[ 'kgf2', 'a', 'b', 'kgf2', 'a', 'b' ]
+// console.log(array.copyWithin(1,3));//[ 'kgf2', 'kgf2', 'a', 'b', 'a', 'b' ]
+// console.log(array)//[ 'kgf2', 'kgf2', 'a', 'b', 'a', 'b' ]
+
+
+  
